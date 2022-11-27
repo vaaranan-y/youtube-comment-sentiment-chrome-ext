5631fa309121bafe15cdfd92478cae214955f865
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2022-11-26 23:58:13 -0500

    Small Test of VADER Sentiment Analysis

--- backend/SentimentAnalyzer/views.py
+++ backend/SentimentAnalyzer/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
-from .apps import WebappConfig
+# from .apps import WebappConfig
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+nltk.downloader.download('vader_lexicon')
 
 
 def index(request):
-    return HttpResponse("Welcome to the sentiment analyzer of this app")
+    return HttpResponse("Welcome to the sentiment analyzer of this app!")
 
 
-def get_text_sentiment(self, request):
-    # print(request)
-    # print('text given: ', text)
-    # print('request body: ', request.body)
+def get_text_sentiment(request):
 
     # PERFORM SENTIMENT ANALYSIS HERE
-    params = request.GET.get('comment')
 
-    response = WebappConfig.predictor.predict(params)
+    sentimentResult = 0
 
-    return JsonResponse(response)
+    sampleText = "I am excited and thrilled to use this sentiment analyzer!!!"
+
+    sia = SentimentIntensityAnalyzer()  # the actual analyzer
+    sentimentResult = sia.polarity_scores(sampleText)
+    return HttpResponse("Result of text sentiment here: " + str(sentimentResult['compound']))
