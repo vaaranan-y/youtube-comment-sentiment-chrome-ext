4cb95306840eb277efd3c2f2e04325ee0089b3ca
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2022-12-15 21:22:22 -0500

    now able to give video id in frontend, and get sentiment score

--- popup.js
+++ popup.js
@@ -1,34 +1,57 @@
 var serverhost = "http://127.0.0.1:8000";
 
-const fetchData = async (rawComment) => {
-  let comment = rawComment.replace(" ", "-");
-  var url =
-    serverhost + "/sentiment-analyzer/get-text-sentiment/" + "I-am-excited";
+
+const fetchVideoData = async (videoId) => {
+  var url = serverhost + "/sentiment-analyzer/get-video-sentiment/" + videoId;
+  console.log(url);
   const options = {
     method: "GET",
   };
   const res = {};
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      return data;
-    });
+  let response = await fetch(url, options);
+  let data = await response.json();
+  return data;
 };
+
+// const fetchTextData = async (rawComment) => {
+//   let comment = rawComment.replace(" ", "-");
+//   var url =
+//     serverhost + "/sentiment-analyzer/get-text-sentiment/" + "I-am-excited";
+//   const options = {
+//     method: "GET",
+//   };
+//   const res = {};
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       return data;
+//     });
+// };
 
 let submitButton = document.getElementById("subjectSubmit");
 
-const submitText = () => {
-  let comment = document.getElementById("subjectText").value;
-  console.log(comment);
+// const submitText = () => {
+//   let comment = document.getElementById("subjectText").value;
+//   console.log(comment);
 
-  if (comment) {
-    fetchData(comment).then((response) => {
-      console.log(response);
-    });
+//   if (comment) {
+//     fetchTextData(comment).then((response) => {
+//       console.log(response);
+//     });
+//   }
+// };
+
+const getVideoSentiment = async () => {
+  let videoId = document.getElementById("subjectText").value;
+  console.log(videoId);
+
+  if (videoId) {
+    let response = await fetchVideoData(videoId);
+    console.log(JSON.stringify(response));
   }
 };
 
 if (submitButton) {
   console.log("the submit button exists");
-  submitButton.addEventListener("click", submitText);
+  submitButton.addEventListener("click", getVideoSentiment);
 }
